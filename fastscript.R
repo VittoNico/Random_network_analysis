d743c14045cf9f2db1202a0f65809eb42f4a3d41
VittoNico <154728891+VittoNico@users.noreply.github.com>
2024-01-04 13:49:34 +0100

    Update fastscript.R

--- fastscript.R
+++ fastscript.R
@@ -117,12 +117,13 @@ writeLines(report, "network_analysis_report.html")
 #Produce a report file for visualize the result in R
 report_data <- c(
     "Network Analysis Report",
-    "Number of Nodes: ", vcount(graph), "(the number of Nodes in your Network.)",
-    "Number of Edges: ", ecount(graph), "(the number of Edges connecting the in your Network.)",
-    "Standard Deviation of Degree: ",  sd(degree_info), "(standard deviation of node degrees. High values = heterogeneity in the degree distribution.)",
-    "Mean Closeness Centrality: ", mean(closeness_info), "(average of closeness centrality values across all Nodes. High values = Nodes in the graph are closer to each other in terms of the shortest path length.)",
-    "Mean Betweenness Centrality: ", mean(betweenness_info), "(average betweenness centrality across all nodes in the graph. High values = nodes in the graph have a more crucial role in connecting different parts of the network.)",
-    "Global Clustering Coefficient: ", clustering_info, "(degree to which nodes in a graph tend to cluster together. High values = higher tendency for nodes in the graph to form clusters or groups)"
+    "Number of Nodes:",vcount(graph),"(the number of Nodes in your network.)",
+    "Number of Edges:",ecount(graph),"(the number of Edges connecting the in your network.)",
+    "Mean Degree:",mean(degree_info),"(mean of the degrees of the nodes in the graph. High values = more connected network)",
+    "Standard Deviation of Degree:",sd(degree_info),"(standard deviation of node degrees. High values = heterogeneity in the degree distribution.)",
+    "Mean Closeness Centrality",mean(closeness_info),"(average of closeness centrality values across all Nodes. High values = Nodes in the graph are closer to each other in terms of the shortest path length.)",
+    "Mean Betweenness Centrality:",mean(betweenness_info),"(average betweenness centrality across all nodes in the graph. High values = nodes in the graph have a more crucial role in connecting different parts of the network.)",
+    "Global Clustering Coefficient:",clustering_info,"(degree to which nodes in a graph tend to cluster together. High values = higher tendency for nodes in the graph to form clusters or groups)"
 )
 cat(report_data, sep = "\n")
 
